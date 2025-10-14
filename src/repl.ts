27e4fb2344fe987a174f6a38fcb7d180561f7e8d
AlexRFarnes <alexrfarnes@gmail.com>
2025-10-15 07:00:28 +0800

    add the exit and help commands

--- src/repl.ts
+++ src/repl.ts
@@ -1,7 +1,39 @@
+import { createInterface } from "readline";
+import { getCommands } from "./command.js";
+
 export function cleanInput(input: string): string[] {
   return input
     .toLowerCase()
     .trim()
     .split(" ")
     .filter(item => item !== "");
+}
+
+export function startRepl() {
+  const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "Pokedex > ",
+  });
+
+  rl.prompt();
+
+  rl.on("line", line => {
+    const words = cleanInput(line);
+
+    if (words.length === 0) {
+      rl.prompt();
+      return;
+    }
+    const command = words[0];
+
+    const commands = getCommands();
+    if (commands[command]) {
+      commands[command].callback(commands);
+    } else {
+      console.log(`Unknown command: ${command}`);
+    }
+
+    rl.prompt();
+  });
 }
